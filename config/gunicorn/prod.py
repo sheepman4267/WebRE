6dfe48a60214531248f37f1ae7ec02cb38a4563d
Hans K <hans@communitycomputing.net>
2022-02-05 19:11:14 -0500

    Tell Gunicorn to let us know what's happening

--- config/gunicorn/prod.py
+++ config/gunicorn/prod.py
@@ -11,8 +11,8 @@ reload = False
 # Write access and error info to /var/log
 accesslog = errorlog = "/var/log/gunicorn/prod.log"
 # Redirect stdout/stderr to log file
-capture_output = True
+capture_output = False
 # PID file so you can easily fetch process ID
 pidfile = "/var/run/gunicorn/dev.pid"
 # Daemonize the Gunicorn process (detach & enter background)
-daemon = True
+daemon = False
