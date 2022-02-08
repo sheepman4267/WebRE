20bd3fa5aa1cfc96b13173ab93fb4dfdbe4d2766
Hans K <hans@communitycomputing.net>
2022-02-07 23:08:51 -0500

    Oh, please. Listen to env var options in both gunicorn configs...

--- config/gunicorn/prod.py
+++ config/gunicorn/prod.py
@@ -5,14 +5,19 @@ loglevel = "warn"
 # The number of worker processes for handling requests
 workers = 2
 # The socket to bind
-bind = "0.0.0.0:8000"
+try:
+    bind = os.environ['GUNICORN_BIND_ADDRESS']
+except IndexError:
+    print("You didn't specify a bind address and port. Use the environment variable 'GUNICORN_BIND_ADDRESS' with the format '0.0.0.0:8000'.")
+    bind = None
+    exit(1)
 # Restart workers when code changes (development only!)
 reload = False
 # Write access and error info to /var/log
-accesslog = errorlog = "/var/log/gunicorn/prod.log"
+accesslog = errorlog = f"/var/log/gunicorn/{os.environ['GUNICORN_PID_LOG_PREFIX']}.log"
 # Redirect stdout/stderr to log file
 capture_output = False
 # PID file so you can easily fetch process ID
-pidfile = "/var/run/gunicorn/dev.pid"
+pidfile = f"/var/run/gunicorn/{os.environ['GUNICORN_PID_LOG_PREFIX']}.pid"
 # Daemonize the Gunicorn process (detach & enter background)
 daemon = False
